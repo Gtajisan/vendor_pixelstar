fbffe08fc47c9625685c3259f579541be194446e
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2024-10-02 21:20:25 +0600

    Update common.mk

--- config/common.mk
+++ config/common.mk
@@ -82,7 +82,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.control_privapp_permissions=enforce
 
 # Google Apps
-$(call inherit-product, vendor/google/gms/products/gms.mk)
+#$(call inherit-product, vendor/google/gms/products/gms.mk)
 
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
